d9ef27d44eeb85b393b2e5fa3a731b155a8c46c8
thang.nguyen <thang.nguyen@holdstastion.com>
2023-06-16 10:07:02 +0700

    fix: welcome html

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { Hono } from "hono";
 import balances from "./apps/balances/routes";
 import * as dotenv from "dotenv";
 import rewards from "./apps/rewards/routes";
+import { welcomeHtml } from "@libsconstants";
 dotenv.config({ path: process.cwd() + "/.env" });
 const app = new Hono();
-app.get("/", (c) => c.text("Welcome to Hono 🔥🔥🔥!"));
+app.get("/", (c) => c.html(welcomeHtml));
 app.route("/balances", balances);
 app.route("/rewards", rewards);
 serve({ fetch: app.fetch, port: +(process.env.PORT || 3000) }, (info) => {
